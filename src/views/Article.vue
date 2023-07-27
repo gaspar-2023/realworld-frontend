97ac5bff66fafcd782c76db46c7aa1af40a571a0
gaspar <gaspar@126.com>
2023-07-27 09:09:08 +0800

    对Article组件进行了三级拆分,Article是一级组件

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,46 +1,36 @@
 <template>
-    <div class="home-page">
-      <div class="banner">
-        <div class="container">
-          <h1 class="logo-font">
-            conduit
-          </h1>
-          <p>A place to share your knowledge.</p>
+  <div class="article-page">
+    <Suspense>
+      <ArticleDetail />
+      <template #fallback>
+        <div class="container page">
+          Article is downloading...
+        </div>
+      </template>
+    </Suspense>
+
+    <Suspense>
+      <div class="row">
+        <div class="col-xs-12 col-md-8 offset-md-2">
+          <ArticleDetailComments />
         </div>
       </div>
-  
-      <div class="container page">
-        <div class="row">
-          <div class="col-md-9">
-            <Suspense>
-              <ArticlesList
-                use-global-feed
-                use-my-feed
-                use-tag-feed
-              />
-              <template #fallback>
-                Articles are downloading...
-              </template>
-            </Suspense>
-          </div>
-  
-          <div class="col-md-3">
-            <div class="sidebar">
-              <Suspense>
-                <PopularTags />
-                <template #fallback>
-                  Popular tags are downloading...
-                </template>
-              </Suspense>
-            </div>
-          </div>
+      <template #fallback>
+        <div class="container page">
+          Comments are downloading...
         </div>
-      </div>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import ArticlesList from 'src/components/ArticlesList.vue'
-  import PopularTags from 'src/components/PopularTags.vue'
-  </script>
-  
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ArticleDetail from 'src/components/ArticleDetail.vue'
+import ArticleDetailComments from 'src/components/ArticleDetailComments.vue'
+</script>
+
+<style scoped>
+.row{
+  margin-right: 0;
+}
+</style>
